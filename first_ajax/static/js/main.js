20ace11fb9c60bd78e3a1387c3cb54eb55526eaf
Anton <anton.mois33v@gmail.com>
2019-03-28 17:00:35 -0400

    Completed assignment

--- first_ajax/static/js/main.js
+++ first_ajax/static/js/main.js
@@ -1,3 +1,49 @@
 document.addEventListener("DOMContentLoaded", function() {
-  
+  const rootBtn = document.getElementById('ajax_to_root');
+  const pingPongBtn = document.getElementById('ajax_to_ping_pong');
+  const countBtn = document.getElementById('ajax_to_count');
+  const timeBtn = document.getElementById('ajax_to_time');
+  const carBtn = document.getElementById('ajax_to_car');
+  const responseSection = document.getElementById('responses');
+  const ourList = document.getElementById('our_list');
+
+  // adding event listeners
+  rootBtn.addEventListener('click', function() {
+    axios.get('http://intro-ajax-api.herokuapp.com/');
+  });
+  pingPongBtn.addEventListener('click', function() {
+    axios.get('http://intro-ajax-api.herokuapp.com/ping').then(function(response) {
+      // ping response
+      console.log(response.data);
+      const newDiv = document.createElement('div');
+      newDiv.innerText = response.data;
+      responseSection.append(newDiv);
+    }).catch(function (error) {
+      // pong response
+      const newDiv = document.createElement('div');
+      newDiv.innerText = error.response.data;
+      responseSection.append(newDiv);
+    }).then(function (response) {
+      console.log("Hey the request finished!");
+    })
+  });
+  countBtn.addEventListener('click', function() {
+    axios.get('http://intro-ajax-api.herokuapp.com/count').then(function(response) {
+      const newDiv = document.createElement('div');
+      newDiv.innerText = response.data;
+      responseSection.append(newDiv);
+    });
+  });
+  timeBtn.addEventListener('click', function() {
+    axios.get('http://intro-ajax-api.herokuapp.com/time', {params:{timezone:'America/Mexico_City'}}).then(function(response) {
+      const newDiv = document.createElement('div');
+      newDiv.innerText = response.data;
+      responseSection.append(newDiv);
+    });
+  });
+  carBtn.addEventListener('click', function() {
+    axios.get('http://intro-ajax-api.herokuapp.com/a_car', {params:{timezone:'America/Mexico_City'}}).then(function(response) {
+      ourList.insertAdjacentHTML('beforeend', response.data);
+    });
+  });
 });
